f497b8c260060fe2167e0c2b2cec22c4aa666581
Angelg10 <angelguzman97@gmail.com>
2020-04-15 16:50:51 -0400

    added image data

--- src/APOD/APOD.js
+++ src/APOD/APOD.js
@@ -4,17 +4,22 @@ import axios from 'axios';
 
 const api_key = 'eWgjUjAhcF03aN85v60ftt7pyn1cnxOxTwn7GhuU'
 
-function APODimage ( {picture, setPicture} ) {
+function APODimage ( {picture, setPicture, pictureTitle, setPictureTitle, pictureCopyright, setPictureCopyright, pictureDate, setPictureDate, pictureExplanation, setPictureExplanation } ) {
 
     useEffect(() => {
         axios.get(`https://api.nasa.gov/planetary/apod?api_key=${api_key}`)
         .then(res => {
           setPicture(res.data.url)
+          setPictureTitle(res.data.title)
+          setPictureCopyright(res.data.copyright)
+          setPictureDate(res.data.date)
+          setPictureExplanation(res.data.explanation)
+        //   debugger
         })
         .catch(err => {
         debugger
         })
-    }, [picture])
+    }, [picture, pictureTitle, pictureCopyright, pictureDate, pictureExplanation])
 
     if (!APODimage) {
         return null
@@ -23,6 +28,10 @@ function APODimage ( {picture, setPicture} ) {
     return ( 
         <div className="APOD-wrapper">
             <img src={picture} width='500' height='350' />
+            <p> {pictureTitle} </p>
+            <p> {pictureCopyright} </p>
+            <p> {pictureDate} </p>
+            <p> {pictureExplanation} </p>
         </div>
     )
 
--- src/App.js
+++ src/App.js
@@ -7,9 +7,17 @@ function App() {
 
   const [picture, setPicture] = useState(null)
 
+  const [pictureTitle, setPictureTitle] = useState(null)
+
+  const [pictureCopyright, setPictureCopyright] = useState(null)
+
+  const [pictureDate, setPictureDate] = useState(null)
+
+  const [pictureExplanation, setPictureExplanation] = useState(null)
+
   return (
     <div className="App">
-      <APOD picture={picture} setPicture={setPicture} />
+      <APOD picture={picture} setPicture={setPicture} pictureTitle={pictureTitle} setPictureTitle={setPictureTitle} pictureCopyright={pictureCopyright} setPictureCopyright={setPictureCopyright} pictureDate={pictureDate} setPictureDate={setPictureDate} pictureExplanation={pictureExplanation} setPictureExplanation={setPictureExplanation} />
     </div>
   );
 }
